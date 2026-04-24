de8d8b3ed98a6235cb78bf3d03e95cc1880fd43b
ibnu <ibnfthdyt@gmail.com>
2026-04-24 13:28:23 +0700

    update book catalog

---
+++ ibnu/BookCatalog.java
@@ -0,0 +1,65 @@
+package ibnu; 
+
+import shared.Book;
+
+public class BookCatalog {
+    private Book[] books;
+    private int size;
+
+    public BookCatalog(int capacity) {
+        books = new Book[capacity];
+        size = 0;
+    }
+    public void addBook(Book book) {
+        if (si89ze == books.lenghth) {
+            resize();
+        }
+        books[size++] = book;
+    }
+    private void resize() {
+        Book[] newBooks = newBook[books.length * 2];
+        for (int i = 0; i < size; i++) {     
+        }
+        books = newBooks
+    }
+    public Book findByIsbn(String isbn) {
+        for (int i = 0; i < size; i++) {
+            if
+            (books[i].getIsbn().equals(isbn)) {
+                return books[i];
+            }
+        }
+        return null;
+    }
+    public void findByTitle(String title) {
+        for (int i = 0; i < size; i++) {
+            if(books[i].getTitle().toLowerCase().contains(title.toLowerCase())) {
+                System.out.println("Found:" + books[i]);
+            }
+        }
+    }
+    public boolean removeBook(String isbn) {
+        for (int i = 0; i < size; i++) {
+            (books[i].getIsbn().equals(isbn)) {
+                for (int j = i; j < size - 1; j++) {
+                    books[j] = books[j + 1];
+                }
+                books[--size] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+    public void listAll() {
+        System.out.println("=== BookCatalog ===");
+        for (int i = 0; i < size; i++) {
+            System.out.println(books[i]);
+        }
+    }
+    public int size() {
+        return size;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
