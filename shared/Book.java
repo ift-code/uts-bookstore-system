c1d2039b95b136ba3884db9de2c5bdca70af2c73
khoerunnisarishy-boop <khoerunnisarishy@gmail.com>
2026-04-24 16:18:57 +0700

    Update project structure and modify Book

--- shared/Book.java
+++ shared/Book.java
@@ -1,39 +1,20 @@
-package shared;
-
 public class Book {
-    private String isbn;
-    private String judul;
-    private String penulis;
-    private double harga;
-    private int stok;
+    String isbn;
+    String title;
+    String author;
+    double price;
+    int stock;
 
-//Constructor
-public Book(String,isbn, String judul, String penulis, double harga, int stok){
-    this.isbn = isbn;
-    this.judul = judul;
-    this.penulis = penulis;
-    this.harga = harga;
-    this.stok = stok;
-}
-//Getter
-public String getIsbn(){
-    return Isbn;
-}
-public String getJudul(){
-    return judul;
-}
-public String getPenulis(){
-    return penulis;
-}
-public double getHarga(){
-    return harga;
-}
-public int getStok(){
-    return stok;
-}
-//Method untuk menampilkan data buku
-@Override
-public String toString(){
-    return "[" + isbn + "] " + judul + " " + penulis + ", Rp" + harga + " (stok: " + stok + ")";
-}
+    public Book(String isbn, String title, String author, double price, int stock) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return title + " by " + author + " (ISBN: " + isbn + ", Price: " + price + ", Stock: " + stock + ")";
+    }
 }
